7c35f7e620915b8b035fd76c9f790e1a6360dcd4
mrwnaziz <84028290+mrwnaziz@users.noreply.github.com>
2023-04-19 12:26:11 +0400

    new

--- pages/index.js
+++ pages/index.js
@@ -44,16 +44,19 @@ export default function Home() {
   return (
     <Fragment>
       <Head>
-        <title>My Awesome GPT-3 App</title>
+        <title>InnovateGPT from MBRCGI</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="bg-gradient-to-r from-pink-100 to-blue-300 min-h-screen px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
         <main className="flex flex-col justify-center  max-w-3xl w-full align-center">
           <h1 className="text-4xl text-center font-extrabold text-slate-800 drop-shadow sm:text-5xl mb-1">
-            My Awesome GPT-3 App...
+            InnovateGPT from MBRCGI
           </h1>
           <p className="block text-sm text-center font-medium text-gray-500">
-            Here you will write your awesome description
+          InnovateGPT is a powerful tool designed to help government innovators find valuable solutions quickly and efficiently, allowing them to focus on their important work of serving the public.
+
+
+n
           </p>
 
           {/* Card & Input field  */}
@@ -63,8 +66,8 @@ export default function Home() {
               type="textarea"
               value={query}
               onChange={(event) => setQuery(event.target.value)}
-              placeholder="Past your article here...."
-              defaultValue={"djuaslkdjh"}
+              placeholder="Write your question here"
+              defaultValue={""}
             />
 
             {/* Button to that calls API */}
